63057d34e87d6c592cf774dc8011a2b3ef9f4b7f
SocialHawk <hagedosd@mail.uc.edu>
2022-02-01 14:50:59 -0500

    Tested Monday and Wednesday activities and took screenshots. Fixed colors in Monday Activity.

--- Activity4/js/main.js
+++ Activity4/js/main.js
@@ -101,7 +101,19 @@ let years = [];
 	  .attr('r',(d) => (d.cost)/165 * (100) + 5 )
 	  .attr('cx',(d) => d.daysFromYrStart/365 * (timelineX2-timelineX1) + timelineX1)
 	  .attr('cy',(d) => (2017 - d.year) * gapBetweenTimelines + startOfTimelinesY )
-	  .attr('fill',"black");
+	  .attr('fill', (d) => {if (d.category == "tropical-cyclone") {
+					return "#081d58";
+				} else if (d.category == "drought-wildfire") {
+					return "#ffffd9";
+				} else if (d.category == "severe-storm") {
+					return "#c7e9b4";
+				} else if (d.category == "winter-storm-freeze") {
+					return "#081d58";
+				} else if (d.category == "flooding") {
+					return "#41b6c4";
+				} else {
+					return "#000000";
+				}});
 
 
 
